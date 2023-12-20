d96013332e9cab69f9a3ae3966303a8d7d716ba0
IvanaFrederic <154450634+IvanaFrederic@users.noreply.github.com>
2023-12-21 00:51:29 +0530

    DFS.py

--- DFS.py
+++ DFS.py
@@ -1,29 +1,34 @@
 class Graph:
     def __init__(self):
-        self.graph = {}
+        self.vertices = {}
 
-    def add_edge(self, vertex, neighbors):
-        self.graph[vertex] = neighbors
+    def add_vertex(self, name, neighbors):
+        self.vertices[name] = neighbors
 
-    def dfs(self, start, visited=None):
-        if visited is None:
-            visited = set()
+    def dfs(self, start, visited, distances):
+        visited.add(start)
+        print(f"Visiting node: {start}, Distance: {distances[start]}")
 
-        if start not in visited:
-            print(start, end=" ")
-            visited.add(start)
-            for neighbor in self.graph[start]:
-                self.dfs(neighbor, visited)
+        for neighbor in self.vertices[start]:
+            if neighbor not in visited:
+                distances[neighbor] = distances[start] + 1
+                self.dfs(neighbor, visited, distances)
+
+    def dfs_traversal(self, start):
+        visited = set()
+        distances = {node: 0 for node in self.vertices}
+        self.dfs(start, visited, distances)
 
 # Example usage:
 g = Graph()
-g.add_edge('A', ['B', 'C'])
-g.add_edge('B', ['A', 'D', 'E'])
-g.add_edge('C', ['A', 'F', 'G'])
-g.add_edge('D', ['B'])
-g.add_edge('E', ['B', 'F'])
-g.add_edge('F', ['C', 'E'])
-g.add_edge('G', ['C'])
+g.add_vertex('A', ['B', 'C'])
+g.add_vertex('B', ['A', 'D', 'E'])
+g.add_vertex('C', ['A', 'F', 'G'])
+g.add_vertex('D', ['B'])
+g.add_vertex('E', ['B'])
+g.add_vertex('F', ['C'])
+g.add_vertex('G', ['C'])
 
-print("DFS starting from vertex 'A':")
-g.dfs('A')
+start_vertex = 'A'
+print("DFS Traversal:")
+g.dfs_traversal(start_vertex)
